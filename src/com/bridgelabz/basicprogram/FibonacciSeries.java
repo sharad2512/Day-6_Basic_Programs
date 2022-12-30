7103d5748c38af92919616b043e982455ee76353
sharad2512 <shnarute95@gmail.com>
2022-12-30 12:00:27 +0530

    ADDED : Code to print fibonacci series program

---
+++ src/com/bridgelabz/basicprogram/FibonacciSeries.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.basicprogram;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+
+        int c;
+        for (int i = 1; i < 10; i++){
+            c = a + b;
+            System.out.println(a+"+"+b);
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+    }
+}
